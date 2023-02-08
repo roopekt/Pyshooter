188ba720592d87155e6e9a6fbe743f0ff37866dc
Roope Keto-Tokoi <roope.keto-tokoi@outlook.com>
2023-02-08 13:48:57 +0200

    cleared some WET code

--- communication.py
+++ communication.py
@@ -4,12 +4,14 @@ import pickle
 from dataclasses import dataclass, field
 import messages
 from random import getrandbits
+from abc import ABC, abstractmethod
 
 PORT = 29801
 PUBLIC_IP = "127.0.0.1" #"195.148.39.50"
 LOCAL_IP = "10.90.77.3"
 MESSAGE_START = b"v!P2"
 RELIABLE_MESSAGE_SEND_COUNT = 3
+RELIABLE_MESSAGE_ID_STORAGE_SIZE = 1024
 
 # all messages start with a header:
 # - constant message start mark (32 bits)
@@ -19,7 +21,7 @@ RELIABLE_MESSAGE_SEND_COUNT = 3
 # - messages sent multiple times
 # - receiver remembers received messages (by id) up to a limit, and only acts on first receival
 
-def get_message(managed_payload):
+def get_packet(managed_payload):
     serialized_data = pickle.dumps(managed_payload)
     content_length_header = len(serialized_data).to_bytes(4, byteorder="big")
     return MESSAGE_START + content_length_header + serialized_data
@@ -75,36 +77,37 @@ class ReliableMessage:
         self.id = getrandbits(32)
         self.payload = payload
 
-class CommunicationServer:
+class CommunicationEndpoint(ABC):
 
-    def __init__(self):
-        self.inwards_message_thread = None
-        self.message_socket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
-        self.message_socket.bind(('', PORT))
-
-        self.connected_players: dict[str, ServerSidePlayerHandle] = {}# ip -> player
+    def __init__(self, message_socket: socket.socket):
+        self.message_socket = message_socket
         self.message_storage = MessageStorage()
+        self.should_run = False
+
+    @abstractmethod
+    def get_reliable_message_id_storage(self, sender_ip) -> ConstSizeQueue:
+        pass
+
+    @abstractmethod
+    def handle_message(self, message):
+        pass
 
     def inwards_message_mainloop(self):
         with self.message_socket as socket:
             while self.should_run:
                 message, address = receive_message(socket)
-                sender = self.connected_players[address]
+                reliable_message_id_storage = self.get_reliable_message_id_storage(address)
 
                 if isinstance(message, ReliableMessage):
-                    if sender.has_received(message):
+                    if message.id in reliable_message_id_storage:
                         continue
                     else:
-                        sender.mark_received_reliable_message(message)
+                        reliable_message_id_storage.add(message.id)
                         message = message.payload
 
-                if type(message) in IMMEDIATE_MESSAGE_HANDLERS:
-                    response = IMMEDIATE_MESSAGE_HANDLERS[type(message)](message, self)
-                else:
-                    self.message_storage.add(message)
+                self.handle_message(message)
 
     def start(self):
-        assert(self.inwards_message_thread == None)
         self.should_run = True
         self.inwards_message_thread = threading.Thread(target=self.inwards_message_mainloop)
         self.inwards_message_thread.start()
@@ -115,63 +118,63 @@ class CommunicationServer:
             self.inwards_message_thread.join()
             self.inwards_message_thread = None
 
-    def send(self, message):
-        self.message_socket.send(get_message(message))
-
-    def send_reliable(self, message):
-        message = get_message(ReliableMessage(message))
-        for i in range(RELIABLE_MESSAGE_SEND_COUNT):
-            self.message_socket.send(message)
-
     def poll_messages(self):
         return self.message_storage.poll()
 
-class CommunicationClient:
+    def enqueue_message(self, message):
+        self.message_storage.add(message)
+
+class CommunicationServer(CommunicationEndpoint):
 
     def __init__(self):
-        self.inwards_message_thread = None
-        self.message_socket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
-        self.message_socket.bind(('', PORT))
+        self.connected_players: dict[str, ServerSidePlayerHandle] = {}# ip -> player
 
-        self.message_storage = MessageStorage()
-        self.received_reliable_message_ids = []
+        message_socket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+        message_socket.bind(('', PORT))
+        super().__init__(message_socket)
 
-    def inwards_message_mainloop(self):
-        with self.message_socket as socket:
-            while self.should_run:
-                message = receive_message(socket)
+    def get_reliable_message_id_storage(self, sender_ip) -> ConstSizeQueue:
+        return self.connected_players[sender_ip].reliable_message_id_storage
 
-                if isinstance(message, ReliableMessage):
-                    if message.id in self.received_reliable_message_ids:
-                        continue
-                    else:
-                        self.received_reliable_message_ids.append(message.id)
-                        message = message.payload
+    def handle_message(self, message):
+        if type(message) in IMMEDIATE_MESSAGE_HANDLERS:
+            response = IMMEDIATE_MESSAGE_HANDLERS[type(message)](message, self)
+        else:
+            self.enqueue_message(message)
 
-                self.message_storage.add(message)
+    def send_to_all(self, message):
+        packet = get_packet(message)
+        for player in self.connected_players.values():
+            self.message_socket.sendto(packet, (player.ip, PORT))
 
-    def start(self):
-        assert(self.inwards_message_thread == None)
-        self.should_run = True
-        self.inwards_message_thread = threading.Thread(target=self.inwards_message_mainloop)
-        self.inwards_message_thread.start()
+    def send_to_all_reliable(self, message):
+        message = ReliableMessage(message)
+        for i in range(RELIABLE_MESSAGE_SEND_COUNT):
+            self.send_to_all(message)
 
-    def stop(self):
-        if self.inwards_message_thread != None:
-            self.should_run = False
-            self.inwards_message_thread.join()
-            self.inwards_message_thread = None
+class CommunicationClient(CommunicationEndpoint):
+
+    def __init__(self):
+        self.reliable_message_id_storage = ConstSizeQueue(RELIABLE_MESSAGE_ID_STORAGE_SIZE)
+
+        message_socket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+        message_socket.bind(('', PORT))
+        super().__init__(message_socket)
+
+    def get_reliable_message_id_storage(self, sender_ip) -> ConstSizeQueue:
+        return super().get_reliable_message_id_storage(sender_ip)
+
+    def handle_message(self, message):
+        self.enqueue_message(message)
 
     def send(self, message):
-        self.message_socket.send(get_message(message))
+        packet = get_packet(message)
+        self.message_socket.sendto(packet, (PUBLIC_IP, PORT))
 
     def send_reliable(self, message):
-        message = get_message(ReliableMessage(message))
+        message = ReliableMessage(message)
         for i in range(RELIABLE_MESSAGE_SEND_COUNT):
-            self.message_socket.send(message)
-
-    def poll_messages(self):
-        return self.message_storage.poll()
+            self.send(message)
 
     def join_server(self):
         self.send_reliable(messages.PlayerConnectionMessage(PUBLIC_IP))
@@ -180,13 +183,7 @@ class ServerSidePlayerHandle:
 
     def __init__(self, player_ip):
         self.ip = player_ip
-        self.received_reliable_message_ids = ConstSizeQueue(1024)
-
-    def has_received(self, reliable_message: ReliableMessage):
-        return reliable_message.id in self.received_reliable_message_ids
-
-    def mark_received_reliable_message(self, reliable_message: ReliableMessage):
-        self.received_reliable_message_ids.add(reliable_message.id)
+        self.reliable_message_id_storage = ConstSizeQueue(RELIABLE_MESSAGE_ID_STORAGE_SIZE)
 
 def handle_PlayerConnectionMessage(message: messages.PlayerConnectionMessage, server: CommunicationServer):
     server.connected_players[message.ip] = ServerSidePlayerHandle(message.ip)

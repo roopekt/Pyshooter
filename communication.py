84986c70457d561cff41531634e1ec9a6c2ae882
Roope Keto-Tokoi <roope.keto-tokoi@outlook.com>
2023-02-23 13:15:06 +0200

    cleand up communication.py

--- communication.py
+++ communication.py
@@ -10,13 +10,10 @@ from dataclasses import dataclass, field
 from time import time, sleep
 from objectid import ObjectId, get_new_object_id
 
-MESSAGE_START = b"v!P2"
 RELIABLE_MESSAGE_INITIAL_SEND_COUNT = 2
 RELIABLE_MESSAGE_ID_STORAGE_SIZE = 1024
 RELIABLE_MESSAGE_RESEND_DELAY = 30 #in milli seconds
 RESEND_MESSAGE_RESEND_ITERATION_DELAY = 15
-
-# all messages start with a constant message start mark (32 bits)
 
 # reliable communicatin protocol:
 # at first, the message (ReliableMessage) is sent multiple times
@@ -68,7 +65,6 @@ class ReliableMessage:
     payload: ...
     id: ObjectId = field(init=False)
 
-
     def __post_init__(self):
         self.id = get_new_object_id()
 
@@ -104,66 +100,59 @@ class UncofirmedMessageStorage:
         with self.lock:
             return list(self.unconfirmed_messages.values())
 
-def get_packet(managed_payload):
-    serialized_data = pickle.dumps(managed_payload)
-    return MESSAGE_START + serialized_data
+class HighLevelSocket:
 
-# returns (data, sender's address)
-def receive_message(socket: socket.socket):
-    data, address = socket.recvfrom(4096)
-    message_start = data[:4]
-    assert(message_start == MESSAGE_START)
+    def __init__(self, low_level_socket: socket.socket):
+        self.socket = low_level_socket
+        self.send_lock = threading.Lock()
 
-    payload = data[4:]
-    message = pickle.loads(payload)
-    return message, address
+    def send_to(self, message, address):
+        packet = pickle.dumps(message)
+        with self.send_lock:
+            if 100 * random() > SIMULATED_PACKAGE_LOSS_PERCENTAGE:
+                self.socket.sendto(packet, address)
+
+    def send_to_reliable(self, message, address, unconfirmed_message_storage: UncofirmedMessageStorage):
+        reliable_message = ReliableMessage(message)
+        unconfirmed_message_storage.add_message(reliable_message, address)
+        for _ in range(RELIABLE_MESSAGE_INITIAL_SEND_COUNT):
+            self.send_to(reliable_message, address)
+
+    # returns (data, sender's address)
+    def receive_message(self):
+        data, address = self.socket.recvfrom(4096)
+        message = pickle.loads(data)
+        return message, address
 
 class CommunicationEndpoint(ThreadOwner, ABC):
 
-    def __init__(self, message_socket: socket.socket, message_storage: ReceivedMessageStorage, thread_name: str):
-        self.message_socket = message_socket
-        self.message_storage = message_storage # unhandled received messages
+    def __init__(self, low_level_socket: socket.socket, resend_thread_name: str):
+        self.message_socket = low_level_socket
+        self.message_storage = ReceivedMessageStorage() # unhandled received messages
         self.unconfirmed_message_storage = UncofirmedMessageStorage() # sent unconfirmed reliable messages
+        self.socket = HighLevelSocket(low_level_socket)
  
         ThreadOwner.__init__(self)
-        self.add_thread(threading.Thread(target=self.inwards_message_mainloop, daemon=True), thread_name)
-        self.add_thread(threading.Thread(target=self.reliable_message_resend_mainloop, daemon=True), f"{thread_name} confirmation")
+        self.add_thread(threading.Thread(target=self.reliable_message_resend_mainloop, daemon=True), resend_thread_name)
         self.send_lock = threading.Lock()
 
-    @abstractmethod
-    def get_reliable_message_id_storage(self, message: ReliableMessage, address) -> ConstSizeQueue:
-        pass
+    def handle_reliable_message(self, reliable_message: ReliableMessage, address, received_reliable_message_id_storage: ConstSizeQueue):
+        self.socket.send_to(MessageConfirmation(reliable_message.id), address)
 
-    @abstractmethod
-    def handle_message(self, message, address):
-        pass
-
-    def inwards_message_mainloop(self):
-        while self.running:
-            message, address = receive_message(self.message_socket)
-
-            if isinstance(message, MessageConfirmation):
-                self.unconfirmed_message_storage.recieve_confirmation(message)
-                continue
-
-            if isinstance(message, ReliableMessage):
-                self.send_to(MessageConfirmation(message.id), address)
-
-                reliable_message_id_storage = self.get_reliable_message_id_storage(message, address)
-                if message.id in reliable_message_id_storage:
-                    continue
-                else:
-                    reliable_message_id_storage.add(message.id)
-                    message = message.payload
-
-            self.handle_message(message, address)
+        if reliable_message.id in received_reliable_message_id_storage:
+            return None
+        else:
+            received_reliable_message_id_storage.add(reliable_message.id)
+            return reliable_message.payload
 
     def reliable_message_resend_mainloop(self):
         while self.running:
             for message in self.unconfirmed_message_storage.get_messages():
                 now = time()
-                if (now - message.last_send_time) > (RELIABLE_MESSAGE_RESEND_DELAY / 1000):
-                    self.send_to(message.message, message.address)
+                time_since_last_send = now - message.last_send_time
+
+                if time_since_last_send > (RELIABLE_MESSAGE_RESEND_DELAY / 1000):
+                    self.socket.send_to(message.message, message.address)
                     message.last_send_time = now
 
             sleep(RESEND_MESSAGE_RESEND_ITERATION_DELAY / 1000)
@@ -174,42 +163,45 @@ class CommunicationEndpoint(ThreadOwner, ABC):
     def enqueue_message(self, message):
         self.message_storage.add(message)
 
-    def send_to(self, message, address):
-        packet = get_packet(message)
-        with self.send_lock:
-            if 100 * random() > SIMULATED_PACKAGE_LOSS_PERCENTAGE:
-                self.message_socket.sendto(packet, address)
-
-    def send_to_reliable(self, message, address):
-        reliable_message = ReliableMessage(message)
-        self.unconfirmed_message_storage.add_message(reliable_message, address)
-        for _ in range(RELIABLE_MESSAGE_INITIAL_SEND_COUNT):
-            self.send_to(reliable_message, address)
-
 class CommunicationServer(CommunicationEndpoint):
 
     def __init__(self, address, start = False):
         self.hosting_client: Optional[HostingCommunicationClient] = None
         self.connected_players: dict[messages.ObjectId, ServerSidePlayerHandle] = {}
-        message_storage = ReceivedMessageStorage()
 
         message_socket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
         message_socket.bind(address)
 
-        super().__init__(message_socket, message_storage, "CommServer")
+        super().__init__(message_socket, "comm-server-resend")
+        self.add_thread(threading.Thread(target=self.inwards_message_mainloop, daemon=True), "comm-server")
 
         if start:
             self.start()
 
-    def get_reliable_message_id_storage(self, message, address) -> ConstSizeQueue:
-        player_id = message.payload.sender_id
-        self.add_player_if_new(player_id, address)
-        return self.connected_players[player_id].reliable_message_id_storage
+    def inwards_message_mainloop(self):
+        while self.running:
+            message, address = self.socket.receive_message()
 
-    def handle_message(self, message, address):
+            if isinstance(message, MessageConfirmation):
+                self.unconfirmed_message_storage.recieve_confirmation(message)
+                continue
+
+            if isinstance(message, ReliableMessage):
+                message = self.handle_reliable_message(message, address, self.get_reliable_message_id_storage(message, address))
+                if message == None:
+                    continue
+
+            self.handle_message(message, address)
+
+    def handle_message(self, message: messages.MessageToServerWithId, address):
         assert(isinstance(message, messages.MessageToServerWithId))
         self.add_player_if_new(message.sender_id, address)
         self.enqueue_message(message)
+
+    def get_reliable_message_id_storage(self, message, address):
+        player_id = message.payload.sender_id
+        self.add_player_if_new(player_id, address)
+        return self.connected_players[player_id].reliable_message_id_storage
 
     def add_player_if_new(self, player_id: messages.ObjectId, address):
         known_ids = list(self.connected_players.keys())
@@ -221,19 +213,19 @@ class CommunicationServer(CommunicationEndpoint):
 
     def send_to_all(self, message):
         for player in self.connected_players.values():
-            self.send_to(message, player.address)
+            self.socket.send_to(message, player.address)
 
         if self.hosting_client != None:
             self.hosting_client.handle_message(message)
 
     def send_to_all_reliable(self, message):
         for player in self.connected_players.values():
-            self.send_to_reliable(message, player.address)
+            self.socket.send_to_reliable(message, player.address, self.unconfirmed_message_storage)
 
         if self.hosting_client != None:
             self.hosting_client.handle_message(message)
 
-class CommunicationClient(ABC, object):
+class CommunicationClient(ABC):
 
     def __init__(self):
         self.id = get_new_object_id()
@@ -254,28 +246,38 @@ class InternetCommunicationClient(CommunicationEndpoint, CommunicationClient):
 
     def __init__(self, own_address, server_address, start = False):
         CommunicationClient.__init__(self)
-        self.reliable_message_id_storage = ConstSizeQueue(RELIABLE_MESSAGE_ID_STORAGE_SIZE)
+        self.reliable_message_id_storage = ConstSizeQueue(RELIABLE_MESSAGE_ID_STORAGE_SIZE) #ids of received reliable messages
         self.server_address = server_address
 
         message_socket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
         message_socket.bind(own_address)
-        CommunicationEndpoint.__init__(self, message_socket, ReceivedMessageStorage(), "InetCommClient")
+        CommunicationEndpoint.__init__(self, message_socket, "inet-comm-client-resend")
+        self.add_thread(threading.Thread(target=self.inwards_message_mainloop, daemon=True), "inet-comm-client")
 
         if start:
             self.start()
 
-    def get_reliable_message_id_storage(self, message, address) -> ConstSizeQueue:
-        return self.reliable_message_id_storage
+    def inwards_message_mainloop(self):
+        while self.running:
+            message, address = self.socket.receive_message()
 
-    def handle_message(self, message, address):
-        assert(isinstance(message, messages.MessageToClient))
-        self.enqueue_message(message)
+            if isinstance(message, MessageConfirmation):
+                self.unconfirmed_message_storage.recieve_confirmation(message)
+                continue
+
+            if isinstance(message, ReliableMessage):
+                message = self.handle_reliable_message(message, address, self.reliable_message_id_storage)
+                if message == None:
+                    continue
+
+            assert(isinstance(message, messages.MessageToClient))
+            self.enqueue_message(message)
 
     def send(self, message):
-        self.send_to(messages.MessageToServerWithId(self.id, message), self.server_address)
+        self.socket.send_to(messages.MessageToServerWithId(self.id, message), self.server_address)
 
     def send_reliable(self, message):
-        self.send_to_reliable(messages.MessageToServerWithId(self.id, message), self.server_address)
+        self.socket.send_to_reliable(messages.MessageToServerWithId(self.id, message), self.server_address, self.unconfirmed_message_storage)
 
 # on the same machine as server, doesn't need internet
 class HostingCommunicationClient(CommunicationClient):

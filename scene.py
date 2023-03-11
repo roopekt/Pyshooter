67f79682be3d5316907bd43ef1cd29952d4066c5
roopekt <roope.keto-tokoi@outlook.com>
2023-03-11 14:16:08 +0200

    Screenshot support F2 saves a picture

--- scene.py
+++ scene.py
@@ -1,6 +1,10 @@
 from abc import ABC, abstractmethod
 import pygame
 from typing import Optional, NewType
+import datetime
+import random
+import pathlib
+from os import makedirs
 
 SceneType = NewType("SceneType", str)
 SCENE_QUIT = SceneType("quit")
@@ -19,12 +23,14 @@ class Scene(ABC):
     def mainloop(self):
         clock = pygame.time.Clock()
         while self.scene_to_switch_to == None:
-            self.delta_time = clock.tick(self.max_fps)
+            self.delta_time = clock.tick(round(self.max_fps))
 
             events = pygame.event.get()
             for event in events:
                 if event.type == pygame.QUIT:
                     self.scene_to_switch_to = SCENE_QUIT
+                elif event.type == pygame.KEYDOWN and event.key == pygame.K_F2:
+                    self.save_screenshot()
 
             self.handle_events(events)
             self.update()
@@ -41,3 +47,12 @@ class Scene(ABC):
     @abstractmethod
     def render(self):
         pass
+
+    def save_screenshot(self):
+        file_name = f"{datetime.datetime.now().strftime('%d-%m-%Y_%H-%M-%S')}_{random.randrange(10**10, 10**11)}.png"
+        save_folder = pathlib.Path.home() / "Pictures" / "Screenshots" / "Pyshooter"
+        save_path = save_folder / file_name
+
+        makedirs(save_folder, exist_ok=True)
+        pygame.image.save(self.window, save_path)
+        print(f'Screenshot saved to "{save_path}"')

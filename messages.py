6ac22a4672f9b2a74e16b555711536c375f4b8f7
Roope Keto-Tokoi <roope.keto-tokoi@outlook.com>
2023-03-21 10:16:37 +0200

    GameMessage & LobbyMessage markers

--- messages.py
+++ messages.py
@@ -10,6 +10,12 @@ class MessageToServer(ABC):
 class MessageToClient(ABC):
     pass
 
+class LobbyMessage(ABC):
+    pass
+
+class GameMessage(ABC):
+    pass
+
 @dataclass
 class MessageToServerWithId:
     sender_id: ObjectId
@@ -18,44 +24,31 @@ class MessageToServerWithId:
 # actual messages:
 
 @dataclass
-class MousePositionUpdate(MessageToServer):
+class MousePositionUpdate(MessageToServer, GameMessage):
     mouse_position_world_space: Vec2d
 
 @dataclass
-class PlayerStateUpdate(MessageToClient):
+class PlayerStateUpdate(MessageToClient, GameMessage):
     player_id: ObjectId
     position: Vec2d
     health: float
     mouse_position_world_space: Vec2d
 
 @dataclass
-class ShootMessage(MessageToServer):
+class ShootMessage(MessageToServer, GameMessage):
     player_position: Vec2d
     mouse_position_world_space: Vec2d
     relative_size: float
 
 @dataclass
-class BulletStateUpdate(MessageToClient):
+class BulletStateUpdate(MessageToClient, GameMessage):
     bullet_id: ObjectId
     position: Vec2d
     radius: float
 
 @dataclass
-class BulletDestroyMessage(MessageToClient):
+class BulletDestroyMessage(MessageToClient, GameMessage):
     bullet_id: ObjectId
-
-@dataclass
-class EnterLobbyMessage(MessageToServer):
-    player_name: str
-
-@dataclass
-class GameStartRequest(MessageToServer):
-    pass
-
-@dataclass
-class LobbyStateUpdate(MessageToClient):
-    connected_player_names: list[str]
-    time_to_game_start: Optional[float]
 
 @dataclass
 class WallUpdate:
@@ -64,5 +57,18 @@ class WallUpdate:
     health: float
     max_health: float
 @dataclass
-class ArenaUpdate(MessageToClient):
+class ArenaUpdate(MessageToClient, GameMessage):
     wall_updates: dict[ObjectId, WallUpdate]
+
+@dataclass
+class EnterLobbyMessage(MessageToServer, LobbyMessage):
+    player_name: str
+
+@dataclass
+class GameStartRequest(MessageToServer, LobbyMessage):
+    pass
+
+@dataclass
+class LobbyStateUpdate(MessageToClient, LobbyMessage):
+    connected_player_names: list[str]
+    time_to_game_start: Optional[float]
